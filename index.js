646882ae3af6fa451478fe1efac81e6630583b62
Mara <125482894+Mara2403@users.noreply.github.com>
2023-10-06 11:57:45 +0200

    update createCalculations, add undo and clear func

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function add(a, b) {
   return a + b;
 }
 
-function substract(a, b) {
+function subtract(a, b) {
   return a - b;
 }
 
@@ -22,34 +22,113 @@ let a = 0;
 let b = 0;
 let operator = "";
 
+let btnEquals = "";
 
-// console.log(add(a, b));
-// console.log(substract(a, b));
-// console.log(multiply(a, b));
-// console.log(divide(a, b));
-
-
-// function operate(num1, num2, operator) {
-//   if (operator === "+") {
-//     return add(num1, num2);
-//   } else if (operator === "-") {
-//     return substract(num1, num2);
-//   } else if (operator === "*") {
-//     return multiply(num1, num2)
-//   } else if (operator === "/") {
-//     return divide(num1, num2);
-//   }
-//   };
 
 function operate(a, b, operator) {
+  a = Number(a);
+  b = Number(b);
+
   if (operator === "+") {
     return add(a, b);
   } else if (operator === "-") {
-    return substract(a, b);
+    return subtract(a, b);
   } else if (operator === "*") {
     return multiply(a, b)
   } else if (operator === "/") {
     return divide(a, b);
   }
   };
-console.log(operate(2, 4, "/"));
+
+
+let button1 = document.querySelector(".btn-1");
+let button2 = document.querySelector(".btn-2");
+let buttonAddition = document.querySelector(".btn-addition");
+let buttonEquals = document.querySelector(".btn-equals");
+let buttonClear = document.querySelector(".btn-clear");
+let buttonUndo = document.querySelector(".btn-undo");
+
+let displayCalculations = document.querySelector(".calculations");
+let displayResult = document.querySelector(".result");
+
+let numericButtons = document.querySelectorAll(".btn-num");
+let operatorButtons = document.querySelectorAll(".btn-operator");
+
+buttonClear.addEventListener("click", function(){
+  displayCalculations.textContent = "";
+  displayResult.textContent = "0";
+});
+
+buttonUndo.addEventListener("click", function(){
+  let string = displayCalculations.textContent;
+  console.log(string);
+  let lastChar = string.charAt(string.length -1);
+  console.log(lastChar);
+    if (lastChar === " ") {
+      displayCalculations.textContent = displayCalculations.textContent.slice(0, -3);
+    } else {
+      displayCalculations.textContent = displayCalculations.textContent.slice(0, -1);
+    }
+  }
+);
+
+
+function createCalculations() { 
+  
+numericButtons.forEach(item => {item.addEventListener("click", function(event) {
+
+displayCalculations.textContent += event.target.value;
+    
+})});
+
+operatorButtons.forEach(item => {item.addEventListener("click", function(event) { 
+ 
+  operator = event.target.value;
+  
+  displayCalculations.textContent += ` ${operator} `;
+
+  if (displayResult.textContent != 0) {
+    displayCalculations.textContent = `${displayResult.textContent} ${operator} `;
+  }
+
+  if (operator === "+" ||
+  operator === "-" ||
+  operator === "*" ||
+  operator === "/") {
+    const grabFirstNumber = displayCalculations.textContent;
+    a = grabFirstNumber.split(" ")[0];
+    console.log("a =", a);
+    console.log("Operator is:", operator);
+    
+
+  };
+  
+})});
+buttonEquals.addEventListener("click", function() {
+  const grabSecondNumber = displayCalculations.textContent;
+  b = grabSecondNumber.substring(grabSecondNumber.lastIndexOf(" ") + 1);
+
+  console.log("b =", b);
+  
+  console.log("Result is:", operate(a, b, operator));
+  displayResult.textContent = parseFloat(operate(a, b, operator).toFixed(2));
+  displayCalculations.textContent = "";
+  a = displayResult.textContent;
+  console.log(a);
+
+
+});
+}
+//trying outside function to create second number and then call it inside createCalculation
+
+function outsideFunctionCreateSecondNumber() {
+  const grabSecondNumber = displayCalculations.textContent;
+  b = grabSecondNumber.substring(grabSecondNumber.lastIndexOf(" ") + 1);
+
+  console.log("b =", b);
+  return b
+}
+
+
+
+createCalculations();
